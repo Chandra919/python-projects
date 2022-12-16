2dc61bc235d748c06d4df1b60102cd4dd6fd2199
Chandra919 <csdeuba123@gmail.com>
2022-12-17 00:54:36 +0545

    blood dnotae

--- training.py
+++ training.py
@@ -1,8 +1,12 @@
-print("Hello")
-x = input("enter value")
-print(x)
-print(type(x))
+a = int(input("enter the age"))
+if a>=18:
+    w = int(input("enter your weight"))
+    if w>=50:
+        print("You can DONATE Congratulations")
+    else:
+        print("Sorry, UNDERWEIGHT")
+else:
+    print("Sorry, UNDERAGE")
 
-
-
-
+    
+    
